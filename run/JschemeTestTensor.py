374fb0ad2367da9913bdcb52b7ef2fe5b4805a0d
Andrew Yeomans-Stephenson <ayeomans@crcfe01.crc.nd.edu>
2026-06-05 13:38:50 -0400

    Stashing

--- run/JschemeTestTensor.py
+++ run/JschemeTestTensor.py
@@ -7,14 +7,14 @@ ms = pyIMSRG.ModelSpace(emax, 'He6', 'He6')
 ut = pyIMSRG.UnitTest(ms)
 passed = True
 #ModelSpace& modelspace, int jrank, int tz, int parity, int particle_rank, int hermitian
-Jx, px, Tx, rankx, hx = 2, 0, 0, 2, +1
-Jy, py, Ty, ranky, hy = 2, 0, 0, 2, -1
+Jx, px, Tx, rankx, hx = 1, 1, 0, 2, +1
+Jy, py, Ty, ranky, hy = 1, 1, 0, 2, -1
 X = ut.RandomOp(ms, Jx, Tx, px, rankx, hx)
 Y = ut.RandomOp(ms, Jy, Ty, py, ranky, hy)
 X.MakeReduced();
 Y.MakeReduced();
 print(X.GetJRank())
-passed &= ut.Mscheme_Test_comm110tt(X,Y)
+passed &= ut.Mscheme_Test_comm220tt(X,Y)
 
 print('passed?', passed)
 prof = pyIMSRG.IMSRGProfiler()
